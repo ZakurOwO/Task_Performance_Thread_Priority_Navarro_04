3f269c4fa94e6df6fbbbf7420f4ffb2a8766c6dd
ZakurOwO <johndanielnavarro130@gmail.com>
2025-09-29 19:10:40 +0800

    Created a Class "MyThreadClass" and added two (2) Threads

---
+++ Task_Performance_Thread_Priority_Navarro_04/MyThreadClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Task_Performance_Thread_Priority_Navarro_04
+{
+    internal class MyThreadClass
+    {
+
+
+        public static void Thread1()
+        {
+            for (int loopCount = 1; loopCount <= 5; loopCount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopCount);
+                Thread.Sleep(500);
+            }
+        }
+
+        public static void Thread2()
+        {
+            for (int loopCount = 1; loopCount <= 5; loopCount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopCount);
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
+
