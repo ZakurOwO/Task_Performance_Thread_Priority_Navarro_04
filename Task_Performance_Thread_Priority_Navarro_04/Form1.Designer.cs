58a12f904ce682c83be9fe37004861ab024024e1
ZakurOwO <johndanielnavarro130@gmail.com>
2025-09-29 18:58:25 +0800

    Add project files.

---
+++ Task_Performance_Thread_Priority_Navarro_04/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Task_Performance_Thread_Priority_Navarro_04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ThreadStatus = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ThreadStatus
+            // 
+            this.ThreadStatus.AutoSize = true;
+            this.ThreadStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.ThreadStatus.Location = new System.Drawing.Point(45, 47);
+            this.ThreadStatus.Name = "ThreadStatus";
+            this.ThreadStatus.Size = new System.Drawing.Size(286, 46);
+            this.ThreadStatus.TabIndex = 0;
+            this.ThreadStatus.Text = "-Thread Starts-";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button1.Location = new System.Drawing.Point(140, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 63);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 249);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ThreadStatus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ThreadStatus;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
